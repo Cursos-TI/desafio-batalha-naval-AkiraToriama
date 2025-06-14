0469d636e3bc15427e55d8ca591723361c1251b8
AkiraToriama <joaovitorpretinho1234@gmail.com>
2025-06-14 16:55:01 +0000

    criei conceitos do jogo batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -22,3 +22,28 @@ int main() {
     int naviosRestantes = 31; // Total de navios (soma dos 1's na matriz)
 
     printf("Batalha naval\n");
+
+    
+    while (tirosRestantes > 0 && naviosRestantes > 0) {
+        printf("Digite a linha e a coluna do tiro (0-9) ou (-1 -1) para sair:\n");
+        scanf("%d %d", &linha, &coluna);
+
+        if (linha == -1 || coluna == -1) {
+            printf("Jogo encerrado pelo usuário.\n");
+            break;
+        }
+
+        if (linha < 0 || linha > 9 || coluna < 0 || coluna > 9) {
+            printf("Tiro fora do tabuleiro!\n");
+        } else if (matriz[linha][coluna] == 0) {
+            printf("Água!\n");
+            matriz[linha][coluna] = -1; // Marca como água já atingida
+            tirosRestantes--;
+        } else if (matriz[linha][coluna] == 1) {
+            printf("Acertou um navio!\n");
+            matriz[linha][coluna] = -2; // Marca navio atingido
+            naviosRestantes--;
+            tirosRestantes--;
+        } else {
+            printf("Você já atirou aqui!\n");
+        }
